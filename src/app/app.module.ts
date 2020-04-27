6c83f25a9c655dab5d7b13f60a473ff12b1200c0
Yastrebov Vladimir <yastrebovvsworks@yandex.ru>
2020-04-28 03:15:18 +0400

    add build docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { AppEffects } from './app.effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+// import { StoreModule } from '@ngrx/store';
+// import { reducers, metaReducers } from './reducers';
+// import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+// import { environment } from '../environments/environment';
+// import { EffectsModule } from '@ngrx/effects';
+// import { AppEffects } from './app.effects';
+// import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { PagesModule } from './pages/shared-pages.module';
 
 @NgModule({
@@ -21,16 +21,16 @@ import { PagesModule } from './pages/shared-pages.module';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true
-      }
-    }),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    EffectsModule.forRoot([AppEffects]),
-    StoreRouterConnectingModule.forRoot(),
+    // StoreModule.forRoot(reducers, {
+    //   metaReducers,
+    //   runtimeChecks: {
+    //     strictStateImmutability: true,
+    //     strictActionImmutability: true
+    //   }
+    // }),
+    // StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    // EffectsModule.forRoot([AppEffects]),
+    // StoreRouterConnectingModule.forRoot(),
     PagesModule
   ],
   providers: [],
